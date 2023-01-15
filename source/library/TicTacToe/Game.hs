0d4742c8fc0ce6a4c3b8382bdb6ccfaf2fdcf278
Felix Springer <felixspringer149@gmail.com>
2023-01-16 00:40:56 +0100

    Add game.

---
+++ source/library/TicTacToe/Game.hs
@@ -0,0 +1,36 @@
+module TicTacToe.Game where
+
+import Data.Kind
+
+type Player :: Type
+data Player
+  = X
+  | Circle
+  deriving stock (Bounded, Enum, Eq, Ord, Read, Show)
+
+type Mark :: Type
+data Mark
+  = Empty
+  | Written Player
+  deriving stock (Eq, Ord, Read, Show)
+
+type Position :: Type
+data Position
+  = TopLeft
+  | TopCenter
+  | TopRight
+  | CenterLeft
+  | CenterCenter
+  | CenterRight
+  | BottomLeft
+  | BottomCenter
+  | BottomRight
+  deriving stock (Bounded, Enum, Eq, Ord, Read, Show)
+
+type Board :: Type
+newtype Board = Fields {readField :: Position -> Mark}
+
+type Move :: Type
+newtype Move = MkMove {getMove :: Position}
+  deriving stock (Eq, Ord, Read, Show)
+  deriving newtype (Bounded, Enum)
